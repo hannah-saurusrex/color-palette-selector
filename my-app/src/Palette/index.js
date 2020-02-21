70cc45bd218091dee30a3db9da4640b582274af2
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-02-20 20:02:34 -0700

    added color palette

---
+++ my-app/src/Palette/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PaletteColor from './PaletteColor.js';
+
+var Palette = props => {
+    var palette = props.palette.map(color => (
+        <PaletteColor />
+    ));
+    return (
+        <div>
+            <h2>Color Palette</h2>
+            <ul className='palette-colors'>{palette}</ul>
+            <hr />
+        </div>
+    );
+};
+
+export default Palette;
