bbfa19856b108e91b1ec4d4914a7b36398689f9c
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-02-20 20:11:54 -0700

    added on click to css option to add in new color

--- my-app/src/CssOptions/CssOption.js
+++ my-app/src/CssOptions/CssOption.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 var CssOption = props => (
-    <li className='color-option'></li>
+    <li className='color-option' onClick={() => props.selectColor(props.color)}>
+        <span
+            className='color-option-preview'
+            style={{ backgroundColor: props.color }}
+        />
+        {props.color}
+    </li>
 );
 
 export default CssOption;
